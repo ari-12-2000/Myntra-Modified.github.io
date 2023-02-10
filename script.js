bc3ca1d12ba1f9961fea5376262ed2ebdd081191
Aritra <arikum12000@gmail.com>
2023-02-10 23:46:22 +0530

    UI UPDATE

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 var s;
 var r;
-
+var support=document.getElementById("bell-icon");
 function toggleNotifi(obj) {
-  
-  if (obj.id.slice(-3)== "div"){
-    s= obj.id.slice(0, -3); 
-    r=document.getElementById(obj.id);
+  if (obj.id.slice(-3) == "div") {
+    s = obj.id.slice(0, -3);
+    r = document.getElementById(obj.id);
+  } else {
+    r = document.getElementById(obj.id + "div");
   }
-  else{
-    r=document.getElementById(obj.id+"div");
-  }
-  r.style.setProperty('--after', '1');
-  document.getElementById("cover").style.display = "block";
+  // if(s=="bell"){
+  //   support.style.setProperty("--visible", "flex");
+  // }
+ 
     document.getElementById(s).style.display = "flex";
+ 
+  r.style.setProperty("--after", "1");
+
+  document.getElementById("cover").style.display = "block";
   
 }
 
 function removeNotifi(obj) {
-  
-  if (obj.id.slice(-3)== "div"){
-    s= obj.id.slice(0, -3); 
-    r=document.getElementById(obj.id);
+  if (obj.id.slice(-3) == "div") {
+    s = obj.id.slice(0, -3);
+    r = document.getElementById(obj.id);
+  } else {
+    r = document.getElementById(obj.id + "div");
   }
-  else{
-    r=document.getElementById(obj.id+"div");
-  }
-  r.style.setProperty('--after', '0');
+  // if(s=="bell"){
+  //   support.style.setProperty("--visible", "none");
+  // }
+  r.style.setProperty("--after", "0");
   document.getElementById("cover").style.display = "none";
-    document.getElementById(s).style.display = "none";
+  document.getElementById(s).style.display = "none";
 }
