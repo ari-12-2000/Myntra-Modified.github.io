fdbcb8fa8ceb187c1142ae940c3520b775f49588
Aritra <arikum12000@gmail.com>
2023-02-14 01:52:19 +0530

    UI UPDATE

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 var s;
 var r;
-var support=document.getElementById("bell-icon");
+const el = document.getElementById("ul");
+
+const style = window.getComputedStyle(el);
+
 function toggleNotifi(obj) {
+
+  
   if (obj.id.slice(-3) == "div") {
     s = obj.id.slice(0, -3);
     r = document.getElementById(obj.id);
   } else {
     r = document.getElementById(obj.id + "div");
   }
- 
-    document.getElementById(s).style.display = "flex";
- 
-  r.style.setProperty("--after", "1");
+  document.getElementById(s).style.display = "flex";
+  if(s!="bell")
+    document.getElementById('ul').style.display = "flex";
 
+  if (r != "uldiv") r.style.setProperty("--after", "1");
   document.getElementById("cover").style.display = "block";
-  
 }
 
 function removeNotifi(obj) {
+
   if (obj.id.slice(-3) == "div") {
     s = obj.id.slice(0, -3);
     r = document.getElementById(obj.id);
   } else {
     r = document.getElementById(obj.id + "div");
   }
-
-  r.style.setProperty("--after", "0");
-  document.getElementById("cover").style.display = "none";
   document.getElementById(s).style.display = "none";
-}
-
-window.onload=function() {
-  console.log(window.innerWidth);
-  if(window.innerWidth<='768' && window.innerHeight<='1024'){
-    document.getElementById('materials').style.height="30vh";
-    
+  if(s!="bell" && style.getPropertyValue('flex-direction')=="column") { 
+    document.getElementById('ul').style.display = "none";
   }
-
+  if (r != "uldiv") r.style.setProperty("--after", "0");
+  document.getElementById("cover").style.display = "none";
 }
